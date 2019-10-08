08f85bee177a5292f6766090be2561bcd19537d7
gpatt9 <gpatt@outlook.com>
2019-10-08 22:28:01 +1000

    implemented connection-based single client message server, without commands

--- client.c
+++ client.c
@@ -1,7 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(){
+#define BUFFER_SIZE 1024
+
+int sockfd;
+
+// safely exit client process
+void clean_exit(int sig){
+	
 	// TODO
-	//
+
+	close(sockfd);
+	exit(1);
+}
+
+int main(int argc, char **argv){
+
+	signal(SIGINT, clean_exit);
+
+	char buffer[BUFFER_SIZE];
+	
+	// expect 2 input arguments
+	if (argc < 3) {
+		printf("Please provide hostname and port.\n");	
+		exit(1);
+	}
+	// assign hostname and port values
+	char* server_name = argv[1];
+	int PORT = atoi(argv[2]);
+	
+	// create client socket
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		fprintf(stderr, "Failed to create socket\n");
+		exit(1);	
+	}
+
+	// setup struct of server address and port
+	struct sockaddr_in server_addr;
+	inet_pton(AF_INET, server_name, &server_addr.sin_addr);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+
+	// connect to server socket
+	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+		fprintf(stderr, "Failed to connect.\n");
+		close(sockfd);
+		exit(1);
+	}
+
+	// read and print welcome message from server
+	read(sockfd, buffer, BUFFER_SIZE);
+	printf("%s", buffer);
+
+	while (1){
+		// TODO
+	}
+
 }
--- server.c
+++ server.c
@@ -1,63 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netinet/in.h>
 
+#define BUFFER_SIZE 1024
 #define QUEUE_SIZE 10
 
-int socket_fd;
+int sockfd;
 
-void clean_shutdown();
-
-int main(int argc, const char** argv){
-
-	// exit signal handler
-	signal(SIGINT,clean_shutdown);
-
-	// set port number
-	uint16_t PORT_NUMBER = 12345;
-	if (argc == 2){ PORT_NUMBER = 12345; }
-
-	// setup server address and port
-	struct sockaddr_in server_addr;
-	int addr_length = sizeof(server_addr);	
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(PORT_NUMBER);
-
-	// create socket file descriptor
-	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-		fprintf(stderr, "Failed to create listen socket\n");
-		exit(1);
-	}
-
-	// bind server address and port to socket
-	if (bind(socket_fd, (struct sockaddr *)&server_addr, addr_length) == -1) {
-		fprintf(stderr, "Failed to bind socket\n");
-		close(socket_fd);
-		exit(1);
-	}
-
-
-	// listen
-	if (listen(socket_fd, QUEUE_SIZE) == -1) {
-		fprintf(stderr, "Failed to listen on socket\n");
-		close(socket_fd);
-		exit(1);
-	}	
-
-
-
-	// main loop
-	while(1){
-	
-	}
-}
-
-void clean_shutdown(int signum){
+void clean_exit(int signum){
 	
 	/* TODO
 
@@ -67,9 +22,66 @@ void clean_shutdown(int signum){
 	
 	*/
 
-	close(socket_fd);
+	close(sockfd);
 	exit(1);
 
 }
 
+int main(int argc, const char** argv){
+
+
+	// exit signal handler
+	signal(SIGINT,clean_exit);
+
+	// set port number
+	uint16_t PORT_NUMBER = 12345;
+	if (argc == 2){ PORT_NUMBER = atoi(argv[1]); }
+
+	// setup server address and port
+	struct sockaddr_in addr;
+	int addrlen = sizeof(addr);	
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(PORT_NUMBER);
+
+	// create socket file descriptor
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		fprintf(stderr, "Failed to create listen socket\n");
+		exit(1);
+	}
+
+	// bind server address and port to socket
+	if (bind(sockfd, (struct sockaddr *)&addr, addrlen) == -1) {
+		fprintf(stderr, "Failed to bind socket\n");
+		close(sockfd);
+		exit(1);
+	}
+
+
+	// set to listen for clients
+	if (listen(sockfd, QUEUE_SIZE) == -1) {
+		fprintf(stderr, "Failed to listen on socket\n");
+		close(sockfd);
+		exit(1);
+	}	
+
+	int num_clients = 0; // number of clients
+
+	// main loop
+	while(1){
+		int client_fd = accept(sockfd, (struct sockaddr *)&addr, (socklen_t *)&addrlen);
+		if (client_fd == -1){
+			perror("accept");
+			continue;
+		}
+		
+		num_clients++;
+
+		char welcome_message[BUFFER_SIZE];
+		sprintf(welcome_message, "Welcome! Your client ID is %d\n", num_clients);
+
+		send(client_fd, welcome_message, strlen(welcome_message), 0);
+
+	}
+}
 
