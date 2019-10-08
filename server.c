7c203d6e7de9d10932b0adcb396fce222ae094ee
gpatt9 <gpatt@outlook.com>
2019-10-08 15:16:14 +1000

    partial implementation of server socket

--- server.c
+++ server.c
@@ -1,8 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
-void shutdown(int signum){
+int socket_fd;
+
+void clean_shutdown();
+
+int main(int argc, const char** argv){
+
+	// exit signal handler
+	signal(SIGINT,clean_shutdown);
+
+
+	// set port number
+	uint16_t PORT_NUMBER = 12345;
+	if (argc == 2){ PORT_NUMBER = 12345; }
+
+	struct sockaddr_in server_addr;
+	int addr_length = sizeof(server_addr);	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(PORT_NUMBER);
+
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (bind(socket_fd, (struct sockaddr *)&server_addr, addr_length) == -1) {
+		fprintf(stderr, "Failed to bind socket\n");
+		clean_shutdown(SIGINT);
+	}
+
+	// main loop
+	while(1){
+	
+	}
+}
+
+void clean_shutdown(int signum){
 	
 	/* TODO
 
@@ -12,18 +49,9 @@ void shutdown(int signum){
 	
 	*/
 
-	printf("test\n");
-	exit(0);
+	close(socket_fd);
+	exit(1);
 
 }
 
-int main(int argc, const char** argv){
 
-
-	signal(SIGINT,shutdown);
-
-	// main loop
-	while(1){
-		
-	}
-}
